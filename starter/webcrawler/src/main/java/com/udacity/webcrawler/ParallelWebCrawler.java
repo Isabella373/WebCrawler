d11cc7f97be75c72a6867004556256c1014ac7ce
Isabella373 <99710738+Isabella373@users.noreply.github.com>
2022-10-21 11:29:29 -0700

    Oct

--- starter/webcrawler/src/main/java/com/udacity/webcrawler/ParallelWebCrawler.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/ParallelWebCrawler.java
@@ -14,6 +14,12 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.ForkJoinPool;
 import java.util.stream.Collectors;
+import com.udacity.webcrawler.parser.PageParser;
+import com.udacity.webcrawler.parser.PageParserFactory;
+
+
+
+import java.util.concurrent.ReceusiveAction;
 
 /**
  * A concrete implementation of {@link WebCrawler} that runs multiple threads on a
@@ -24,23 +30,82 @@ final class ParallelWebCrawler implements WebCrawler {
   private final Duration timeout;
   private final int popularWordCount;
   private final ForkJoinPool pool;
+  private final PageParserFactory parserFactory;
 
   @Inject
   ParallelWebCrawler(
       Clock clock,
+      PageParserFactory parserFactory,
       @Timeout Duration timeout,
       @PopularWordCount int popularWordCount,
       @TargetParallelism int threadCount) {
     this.clock = clock;
     this.timeout = timeout;
+    this.parserFactory = parserFactory;
     this.popularWordCount = popularWordCount;
     this.pool = new ForkJoinPool(Math.min(threadCount, getMaxParallelism()));
   }
 
   @Override
   public CrawlResult crawl(List<String> startingUrls) {
-    return new CrawlResult.Builder().build();
+    Instant deadline = clock.instant().plus(timeout);
+    Map<String, Integer> counts = Collections.synchronizedSet(new HashMap<>());
+    Set<String> visitedUrls = Collections.synchronizedSet(new HashSet<>());
+    for (String url : startingUrls) {
+      pool.invoke(new CrawParallel(url, deadline, counts, visitedUrls));
+    }
+
+
+    if (counts.isEmpty()) {
+      return new CrawlResult.Builder()
+              .setWordCounts(counts)
+              .setUrlsVisited(visitedUrls.size())
+              .build();
+    }
+
+    return new CrawlResult.Builder()
+            .setWordCounts(WordCounts.sort(counts, popularWordCount))
+            .setUrlsVisited(visitedUrls.size())
+            .build();
   }
+
+  private class CrawParallel extends RecursiveAction {
+    final String url;
+    Instant deadline;
+    int threadCount;
+    Map<String, Integer> counts;
+    Set<String> visitedUrls;
+
+    CrawParallel(String url, Instant deadline, Map<String, Integer> counts, Set<String> visitedUrls){
+      this.url = url;
+      this.deadline = deadline;
+      this.counts = counts;
+      this.visitedUrls = visitedUrls;
+
+  }
+    @Override
+    protected void compute(){
+      if (threadCount == 0 || clock.instant().isAfter(deadline)) {
+        return;
+      }
+      if (visitedUrls.contains(url)) {
+        return;
+      }
+      visitedUrls.add(url);
+      PageParser.Result result = parserFactory.get(url).parse();
+
+    for (Map.Entry<String, Integer> e : result.getWordCounts().entrySet()) {
+      if (counts.containsKey(e.getKey())) {
+        counts.put(e.getKey(), e.getValue() + counts.get(e.getKey()));
+      } else {
+        counts.put(e.getKey(), e.getValue());
+      }
+    }
+
+    for (String link : result.getLinks()) {
+      invokeAll(new CrawParallel(link, deadline, counts,visitedUrls));
+    }}}
+
 
   @Override
   public int getMaxParallelism() {
